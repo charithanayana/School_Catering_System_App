dc409c0a862d254035f60b12554db4662e05f2b4
charithanayana <charithanayana@gmail.com>
2024-03-29 21:55:49 +0530

    BMI details initial implementation

--- src/pages/schoolAdministration/BMIdetail.tsx
+++ src/pages/schoolAdministration/BMIdetail.tsx
@@ -1,42 +1,100 @@
-import { Box, Card, CardContent, Typography } from '@mui/material';
-import Grid from '@mui/material/Grid';
 import SchoolAdminSideNav from '../../components/SchoolAdminSideNav';
+import { Box, Button, Grid, Link, TextField, Typography, Select, MenuItem, InputLabel, FormControl, SelectChangeEvent } from '@mui/material';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import { useNavigate } from "react-router-dom";
+import { LineChart } from '@mui/x-charts/LineChart';
 
 function BMIdetail() {
+
+  const navigate = useNavigate();
+
+  const [students, setStudents] = useState<any[]>([]);
+  const [xAxisArray, setXaxisArray] = useState<any[]>([]);
+  const [bmiArray, setBmiArray] = useState<any[]>([]);
+
+  useEffect(() => {
+    const res = axios.get("http://localhost:8080/catering/students", {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      setStudents(response.data);
+    });
+  }, []);
+
+  const handleChange = (event: SelectChangeEvent) => {
+    let studentId = event.target.value;
+    const res = axios.get("http://localhost:8080/catering/students/report/" + studentId, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      let xAxisArr = [];
+      let bmiArr = [];
+      let data = response.data;
+      for (var ind in data) {
+        xAxisArr.push(data[ind].dateString);
+        bmiArr.push(data[ind].bmi);
+      }
+      setXaxisArray(xAxisArr);
+      setBmiArray(bmiArr);
+    });
+  };
+
   return (
     <>
-      <Box sx={{ display: 'flex', marginTop: '60px' }}>
-        <SchoolAdminSideNav title='BMI Details'/>
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={8}>
-              <Card sx={{ maxWidth: 345 }}>
-             
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Lizard
+        <Box sx={{ display: 'flex', marginTop: '60px' }}>
+            <SchoolAdminSideNav title='BMI Details'/>
+            <Box component="div" sx={{ flexGrow: 1, p: 3 }}>
+              <Box textAlign={'center'}>
+                  <Typography lineHeight={3} component="h1" variant="h4">
+                      BMI Details
                   </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                   School Administration BMI Functions
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={4}>
+              </Box>
+              <Box component="div">
+                <Grid container spacing={2} rowSpacing={5} marginTop={5}>
+                    <Grid item xs={6} sm={6}>                         
+                      <FormControl sx={{ width: 1 }}>
+                        <InputLabel id="demo-simple-select-helper-label">Student</InputLabel>
+                        <Select
+                          labelId="demo-simple-select-helper-label"
+                          id="demo-simple-select-helper"
+                          label="mmmb"
+                          size='small'
+                          name="studentId"
+                          onChange={handleChange}
+                        >
+                          <MenuItem key="-1" value="">Please select</MenuItem>
+                          {students.map((student) => (
+                            <MenuItem value={student.id}>{student.firstName}</MenuItem>
+                          ))}
+                        </Select>
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={6} sm={6} />
 
-            </Grid>
-          </Grid>
-          <Box height={20} />
-          <Grid container spacing={2}>
-            <Grid item xs={8}>
+                    <Grid container spacing={2} rowSpacing={5} marginTop={5}>
+                  <Grid item xs={6} sm={6}>                         
+                    <LineChart
+                      xAxis={[{ data: xAxisArray, scaleType: 'point' }]}
+                      series={[{ data: bmiArray }]}
+                      width={500}
+                      height={300}
+                    />
+                  </Grid>
+                  <Grid item xs={6} sm={6} />
 
-            </Grid>
-            <Grid item xs={4}>
+              </Grid>
 
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
+                </Grid>
+
+
+
+              </Box>
+            </Box>
+          </Box>
     </>
   )
 }
